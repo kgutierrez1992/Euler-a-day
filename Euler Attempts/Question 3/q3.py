06921437e8bf15d2c3748f5c49180b37c4807cbe
Konnor Gutierrez <kgutierrez1992@live.com>
2022-07-28 16:18:45 -0700

    completed question 3

---
+++ Euler Attempts/Question 3/q3.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+# Konnor Gutierrez
+# CPSC 386
+# kgutierrez1992@live.com
+# 
+
+'''This is a sample program for my CPSC 386 course'''
+
+def is_divisible(dividend, divisor):
+    quotient = dividend // divisor
+    if quotient * divisor == dividend:
+        return True
+    return False
+
+
+def main():
+    '''This is the main function.'''
+    original = 600851475143
+    max_val = 600851475143
+    n = 1
+    while n < max_val:
+        if is_divisible(max_val, n):
+            max_val = max_val // n
+        n += 1
+    print(f"The largest prime value for {original} is: {max_val}")
+
+    
+
+if __name__ == '__main__':
+    main()

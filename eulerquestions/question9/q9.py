a3174e377f21ebe72165bfd0ecb36e9aabe1aae0
Konnor Gutierrez <kgutierrez1992@live.com>
2022-08-03 18:14:33 -0700

    solution for q9

--- eulerquestions/question9/q9.py
+++ eulerquestions/question9/q9.py
@@ -8,7 +8,30 @@
 
 def main():
     '''This is the main function.'''
-    print("hello world")
+    
+    result = 0
+    a = 0
+    b = 0
+    c = 0
+    result_arry = []
+    sum_to_find = 1000
+
+    for i in range(500):
+        for j in range(i+ 1, 500):
+            a = j**2 - i**2
+            b = 2 * j * i
+            c = j**2 + i**2
+
+            if a + b + c > sum_to_find:
+                break
+            elif a + b + c == sum_to_find:
+                result_arry.append(a)
+                result_arry.append(b)
+                result_arry.append(c)
+                result = a*b*c
+
+    print(result_arry)
+    print(result)
     
 
 if __name__ == '__main__':

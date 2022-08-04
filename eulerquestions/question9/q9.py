f27dd334ffe7ab07658b237b60b3fffad8e9208b
Konnor Gutierrez <kgutierrez1992@live.com>
2022-08-03 17:48:33 -0700

    changing folder structure

---
+++ eulerquestions/question9/q9.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+# Konnor Gutierrez
+# CPSC 386
+# kgutierrez1992@live.com
+# 
+
+'''This is a sample program for my CPSC 386 course'''
+
+def main():
+    '''This is the main function.'''
+    print("hello world")
+    
+
+if __name__ == '__main__':
+    main()
